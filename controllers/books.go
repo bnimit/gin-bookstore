305d9e3f029a575df4a03104b90505ffc46d6e84
Nimit Bhandari <nimitbhandari@outlook.com>
2022-11-15 03:29:09 +0530

    Add a custom Input object to validate POST request params before creating entries in the database

--- controllers/books.go
+++ controllers/books.go
@@ -7,11 +7,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 	models.DB.Find(&books)
 
 	c.JSON(http.StatusOK, gin.H{
 		"data": books,
+	})
+}
+
+func CreateBook(c *gin.Context) {
+	var input CreateBookInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	//Create book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+	c.JSON(http.StatusOK, gin.H{
+		"data": book,
 	})
 }
