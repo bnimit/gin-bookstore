8059b0b9e51f463767e489da8ca7987eed14f839
Nimit Bhandari <nimitbhandari@outlook.com>
2022-11-15 03:48:58 +0530

    Add a route to GET endpoints for a single book when an ID is provided

--- controllers/books.go
+++ controllers/books.go
@@ -38,3 +38,20 @@ func CreateBook(c *gin.Context) {
 		"data": book,
 	})
 }
+
+// Find a single book
+func FindBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Record not found!",
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": book,
+	})
+}
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	models.ConnectDatabase()
 
 	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
 	r.POST("/books", controllers.CreateBook)
 
 	r.Run()
